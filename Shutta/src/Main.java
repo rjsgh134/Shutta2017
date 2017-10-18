057bb28a67548170bcb1fe9ba38a80500b0e4e22
rjsgh134 <rjsgh134@naver.com>
2017-10-18 15:33:37 +0900

    ok

--- Shutta/src/Main.java
+++ Shutta/src/Main.java
@@ -1,7 +1,8 @@
-//1
+//2
 public class Main {
     public static void main(String[] args) {
         System.out.println("hello");
         System.out.println("aa");
+        System.out.println("good");
     }
 }
