c9323e75daecee539477d9b8115dc91a4e45e2a3
rjsgh134 <rjsgh134@naver.com>
2017-10-18 15:42:23 +0900

    다 지움

--- Shutta/src/Main.java
+++ Shutta/src/Main.java
@@ -1,8 +1,6 @@
-//2
+//
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hello");
-        System.out.println("aa");
-        System.out.println("good");
+
     }
 }
